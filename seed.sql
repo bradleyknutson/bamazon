0cf3f257bc81aa6dd36a7ed1b15d3dda4da55db8
Bradley Knutson <bradley@bknutson.com>
2019-12-28 11:16:53 -0700

    Initial db schema and seed

---
+++ seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("The CS Detective", "Books", 12.99, 35),
+      ("Dune", "Books", 9.79, 10),
+      ("Settler's of Catan", "Board Games", 24.99, 100),
+      ("Pandemic", "Board Games", 29.88, 24),
+      ("Betrayal At House on the Hill", "Board Games", 32.49, 5),
+      ("AMD Ryzen 7 3800 8-Core", "Electronics", 338.99, 13),
+      ("Intel Core i9-9820X 10 Core", "Electronics", 644.24, 26),
+      ("NVIDIA GeForce RTX 2080 Ti", "Electronics", 1498.00, 1),
+      ("XFX Rx 5700", "Electronics", 329.99, 64),
+      ("All-Clad D3 Stainless Cookware Set", "Home & Kitchen", 582.33, 6),
+      ("Sandoo HA1897 Induction Cooktop, 1800W", "Home & Kitchen", 48.99, 12);
